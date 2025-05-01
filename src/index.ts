f2139bb1741a524c121bcb2dcbaf7bc6b90f86b3
Kingsley Ifijeh <kinxly@gmail.com>
2025-05-01 11:34:29 +0100

    refactor: updating components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * from "./components/core/atoms/button";
-export * from "./components/core/atoms/inputs";
-export * from "./components/core/atoms/logo";
+export { CustomButton } from "./components/core/atoms/button";
+export { InputField } from "./components/core/atoms/inputs";
+export { Logo } from "./components/core/atoms/logo";
