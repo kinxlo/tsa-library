f319101b8f020d0832fa3f46c5e0b4b8e3b33458
Kingsley Ifijeh <kinxly@gmail.com>
2025-05-01 12:19:00 +0100

    refactor: updating components

---
+++ vite.config.ts
@@ -0,0 +1,60 @@
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import { UserConfigExport } from "vite";
+import dts from "vite-plugin-dts";
+import { defineConfig } from "vitest/config";
+
+import { name } from "./package.json";
+
+const app = async (): Promise<UserConfigExport> => {
+  /**
+   * Removes everything before the last
+   * @octocat/library-repo -> library-repo
+   * vite-component-library-template -> vite-component-library-template
+   */
+  const formattedName = name.match(/[^/]+$/)?.[0] ?? name;
+
+  return defineConfig({
+    plugins: [
+      react(),
+      dts({
+        insertTypesEntry: true,
+      }),
+    ],
+    // css: {
+    //   postcss: {
+    //     plugins: [tailwindcss],
+    //   },
+    // },
+    resolve: {
+      alias: {
+        "@": path.resolve(import.meta.dirname, "src"),
+      },
+    },
+    build: {
+      lib: {
+        entry: path.resolve(import.meta.dirname, "src/index.ts"),
+        name: formattedName,
+        formats: ["es", "umd"],
+        fileName: (format) => `${formattedName}.${format}.js`,
+      },
+      rollupOptions: {
+        external: ["react", "next", "react/jsx-runtime", "react-dom", "tailwindcss"],
+        output: {
+          globals: {
+            react: "React",
+            "react/jsx-runtime": "react/jsx-runtime",
+            "react-dom": "ReactDOM",
+            tailwindcss: "tailwindcss",
+          },
+        },
+      },
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+    },
+  });
+};
+// https://vitejs.dev/config/
+export default app;
