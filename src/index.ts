a7dadaabeb8e853b8b4b3fd0fb8bc438fb5c138d
Kingsley Ifijeh <kinxly@gmail.com>
2025-05-01 13:34:03 +0100

    refactor: updating components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// ATOMS
 export * from "./components/core/atoms/button";
 export * from "./components/core/atoms/inputs";
 export * from "./components/core/atoms/logo";
+
+// MOLECULES
+export * from "./components/core/molecules/navbar";
